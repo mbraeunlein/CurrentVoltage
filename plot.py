f09444040bb2917aa481f6b1281f31e8f7bcefc1
hwlab <hwlab@ariel.ess.informatik.tu-darmstadt.de>
2014-04-01 12:52:15 +0200

    plot plots spans now and combines successive peeks that are incorrectly detected as multiple peeks with adjustable tolerance range

--- plot.py
+++ plot.py
@@ -1,15 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-foo1 = np.load('data-127.npy')
-foo2 = np.load('data-132.npy')
+filename = sys.argv[1]
+threshold = int(sys.argv[2])
+running = False
+start = 0
+indices = []
+dist = 100
+count = 0
 
+rawData = np.load(filename)
+data = np.concatenate(np.array(rawData)[:,1].flatten())
 
-bla1 = np.concatenate(np.array(foo1)[:,1].flatten())
-bla2 = np.concatenate(np.array(foo2)[:,1].flatten())
+for idx, val in enumerate(data):
+	if val > threshold:
+		if not running:
+			running = True
+			start = idx
+	if val < threshold:
+		if running:
+			if dist > count:
+				count = count + 1
+			else:
+				count = 0
+				running = False
+				indices.append((start, idx - dist))
 
-plt.plot(bla1,'-',label='no. 127')
-plt.plot(bla2,'-',label='no. 132')
-#~ plt.legend()
+for i in indices:
+	if max(data[i[0]:i[1]]) > 700:
+		plt.axvspan(i[0], i[1], facecolor='r', alpha=0.2)
+	else:
+		plt.axvspan(i[0], i[1], facecolor='g', alpha=0.2)
 
+plt.plot(data,'-',label='no. 127')
 plt.show()
