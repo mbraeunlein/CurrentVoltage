da63f42ee8cb82e0fe6022dce451487b429cd4c8
hwlab <hwlab@ariel.ess.informatik.tu-darmstadt.de>
2014-04-10 14:28:09 +0200

    last changes

--- read.py
+++ read.py
@@ -17,19 +17,21 @@ ydata = np.zeros(interval)
 
 port = serial.Serial('/dev/ttyACM0', baudrate, timeout=1)
 
+
 while (float(time.time() - start)) < float(seconds):
 	if int(time.time() - int(start)) > last:
 		print int(seconds) - last
 		last = last + 10
 
 	t = datetime.datetime.now()
-	buf = np.int_([port.readline().strip() for i in range(interval)])
 	
-	#~ x = port.read(interval)
-	#~ buf = struct.unpack('2000B',x)
+	try:
+		buf = np.int_([port.readline().strip() for i in range(interval)])
+		dta.append((t,buf))
+		dtamn.append(np.mean(buf))
+	except:
+		print "buff error"
 	
-	dta.append((t,buf))
-	dtamn.append(np.mean(buf))
 	try:
 		ydata = np.concatenate( (ydata, buf) )[len(buf):]
 	except:
